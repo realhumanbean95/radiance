48397be9fba851c11c873c5e2a39da40a066e569
Luke Tanner <luketanner95@gmail.com>
2022-03-18 14:16:42 -0700

    hello-texture project uses hello-shader's shader header-only library

--- conanfile.py
+++ conanfile.py
@@ -5,13 +5,9 @@ class DeepProject(ConanFile):
     name = "deep"
     version = "0.1.0"
     requires = (
-        "catch2/2.13.7",
         "docopt.cpp/0.6.2",
-        "fmt/8.0.1",
-        "spdlog/1.9.2",
         "glfw/3.3.6",
         "glad/0.1.34",
-        "namedtype/20190324",
         "stb/cci.20210713"
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
