f93bc80fae4f759e9c86d568b4bb22b9354d052c
Luke Tanner <luketanner95@gmail.com>
2022-03-24 18:52:51 -0700

    added a Window abstraction, implemented in terms of GLFW

--- src/radiance/radiance.cpp
+++ src/radiance/radiance.cpp
@@ -3,6 +3,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "window.hpp"
 #include "drawable.hpp"
 #include "factory.hpp"
 #include "shader.hpp"
@@ -12,49 +13,13 @@
 // You can modify the source template at `configured_files/config.hpp.in`.
 #include <configured_files/config.hpp> // use this to get meta information about the build (version, etc)
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
 int main(int argc, const char** argv)
 {
     using namespace radiance;
 
-    std::cout << "Hello Drawable!" << std::endl;
+    std::cout << "Hello Radiance!" << std::endl;
 
-    /* Initialize the library */
-    if (!glfwInit()) {
-        std::cout << "glfwInit failed." << std::endl;
-        return -1;
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Triangle!", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);
+    window::Window window{};
 
     // verts for a triangle
     float vertices1[] = {
@@ -90,29 +55,28 @@ int main(int argc, const char** argv)
     drawable2->setShader(shader::Shader{});
 
 
-    while (!glfwWindowShouldClose(window))
+    while ( !window.shouldClose() )
     {
         // input
-        processInput(window);
+        window.processInput();
 
         //render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         drawable2->bindContext();
-        drawable2->draw();
+        drawable2->draw(); // in OpenGL, render to back buffer
 
         drawable1->bindContext();
-        drawable1->draw();
+        drawable1->draw(); // // in OpenGL, render to back buffer
 
         //call events and swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        window.swapBuffers();
+        window.pollEvents();
 
         // 5 unbind vertex array
         glBindVertexArray(0);
     }
 
-    glfwTerminate();
     return 0;
 }
---
+++ src/window/include/window.hpp
@@ -0,0 +1,84 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.hpp"
+
+namespace radiance::window
+{
+
+class Window
+{
+public:
+    Window()
+    {
+        /* Initialize the library */
+        if (!glfwInit()) {
+            std::cout << "glfwInit failed." << std::endl;
+            //return -1;
+        }
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window = glfwCreateWindow(800, 600, "Hello Radiance!", NULL, NULL);
+        if (window == NULL)
+        {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            //return -1;
+        }
+        glfwMakeContextCurrent(window);
+        
+        // NOTE: framebufferSizeCallback is a static member function.
+        glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            //return -1;
+        }
+
+        glViewport(0, 0, 800, 600);
+
+    }
+    ~Window()
+    {
+        glfwTerminate(); 
+    }
+
+    void processInput()
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+    }
+
+    void swapBuffers()
+    {
+        glfwSwapBuffers(window);
+    }
+
+    void pollEvents()
+    {
+        glfwPollEvents();
+    }
+
+    bool shouldClose()
+    {
+        return glfwWindowShouldClose(window);
+    }
+
+private:
+    // NOTE: made static so it could be passed to GLFW as a function pointer
+    // May cause issues if we ever render multiple windows...not sure if they would share GLFW context etc...
+    static void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+    {
+        glViewport(0, 0, width, height);
+    }
+
+    GLFWwindow* window;
+
+};
+
+
+
+}; // namespace radiance::window
