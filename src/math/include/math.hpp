5b2ee543d71f382482aa861df39d2369e6c66588
Luke Tanner <luketanner95@gmail.com>
2022-03-29 13:39:11 -0700

    some more renaming

--- src/math/include/math.hpp
+++ src/math/include/math.hpp
@@ -15,47 +15,47 @@ namespace radiance::math
     class Mat4
     {
     public:
-        glm::mat4 trans;
 
         Mat4(float* mat4)
         {
-            trans = glm::make_mat4(mat4);
+            _trans = glm::make_mat4(mat4);
         }
 
         Mat4()
         {
-            trans = glm::mat4(1.0f);
+            _trans = glm::mat4(1.0f);
         }
 
-        Mat4 operator*( const Mat4& Mat4_0 )
+        Mat4 operator*( const Mat4& mat4 )
         {
-            glm::mat4 result = trans * Mat4_0.trans;
+            glm::mat4 result = _trans * mat4._trans;
             return Mat4{ glm::value_ptr(result) };
         }
 
         void translate(float* translation_vector)
         {
-            trans = glm::translate(trans, glm::make_vec3(translation_vector));
+            _trans = glm::translate(_trans, glm::make_vec3(translation_vector));
         }
 
         void rotate(float* rotation_vector, float degrees)
         {
-            trans = glm::rotate(trans, degrees, glm::make_vec3(rotation_vector));
+            _trans = glm::rotate(_trans, degrees, glm::make_vec3(rotation_vector));
         }
 
         void scale(float* scaling_vector)
         {
-            trans = glm::scale(trans, glm::make_vec3(scaling_vector));
+            _trans = glm::scale(_trans, glm::make_vec3(scaling_vector));
         }
 
         float* getDataPtr()
         {
-            return glm::value_ptr(trans);
+            return glm::value_ptr(_trans);
         }
 
     private:
+        glm::mat4 _trans;
 
-        Mat4(glm::mat4 _mat4) : trans( _mat4 )
+        Mat4(glm::mat4 mat4) : _trans( mat4 )
         {}
     };
 
