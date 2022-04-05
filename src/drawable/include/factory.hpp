56488a0f4fe14b2824694569b37bb4eff1a35551
Luke Tanner <luketanner95@gmail.com>
2022-04-05 11:41:22 -0700

    DrawableFactory.createDrawable() is now templated on VertexType and IndexType

--- src/drawable/include/factory.hpp
+++ src/drawable/include/factory.hpp
@@ -15,19 +15,20 @@ namespace radiance::drawable
     class DrawableFactory
     {
     public:
+        template<typename VertexType, typename IndexType>
         Drawable* createDrawable(
             VertexSemantic vertexSemantic,
-            float* _vertices, uint32_t* _indices,
-            uint32_t _vertices_size_bytes, uint32_t _indices_size_bytes)
+            VertexType* _vertices, IndexType* _indices,
+            size_t _vertices_size_bytes, size_t _indices_size_bytes)
         {
             switch (vertexSemantic) {
-            case F3POSF3COL_INDEXED: // 3 floats for position, 3 floats for color
+            case F3POSF3COL_INDEXED: // 3 VertexTypes for position, 3 VertexTypes for color
                 return new Drawable_F3POSF3COL_Indexed(_vertices, _indices, _vertices_size_bytes, _indices_size_bytes);
                 break; //optional
-            case F3POS_INDEXED: // 3 floats for position
+            case F3POS_INDEXED: // 3 VertexTypes for position
                 return new Drawable_F3POS_Indexed(_vertices, _indices, _vertices_size_bytes, _indices_size_bytes);
                 break; //optional
-            case F3POSF2TEX_INDEXED: // 3 floats for position, 2 floats for texture coordinates
+            case F3POSF2TEX_INDEXED: // 3 VertexTypes for position, 2 VertexTypes for texture coordinates
                 return new Drawable_F3POSF2TEX_Indexed(_vertices, _indices, _vertices_size_bytes, _indices_size_bytes);
                 break; //optional
 
@@ -38,13 +39,14 @@ namespace radiance::drawable
         }
 
         // overload for instantiating un-indexed drawables
+        template<typename VertexType>
         Drawable* createDrawable(
             VertexSemantic vertexSemantic,
-            float* _vertices,
-            uint32_t _vertices_size_bytes)
+            VertexType* _vertices,
+            size_t _vertices_size_bytes)
         {
             switch (vertexSemantic) {
-            case F3POSF2TEX: // 3 floats for position, 2 floats for texture coordinates, unindexed
+            case F3POSF2TEX: // 3 VertexTypes for position, 2 VertexTypes for texture coordinates, unindexed
                 return new Drawable_F3POSF2TEX(_vertices, _vertices_size_bytes);
                 break; //optional
 
@@ -54,6 +56,5 @@ namespace radiance::drawable
             }
         }
     };
-
 
 }; // namespace radiance::drawable
